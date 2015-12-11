713ebd6564a0e3b2bc193dfd22eeafba07185a62
1945821990 <z0o@outlook.de>
2015-12-11 09:49:55 +0100

    Create lagerbestand.cs

---
+++ lagerbestand.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagerbestandrechnung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double  dlagerb;
+            double us;
+            double dlagerd;
+            double lagerz;
+
+            Console.WriteLine("Bitte geben Sie den Jahresanfangs Bestand ein: ");
+            double jab = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Bitte geben Sie den Jahresbestand ein: ");
+            double jb = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Wieviel Komponenten wurden im Laufe des Jahres entnommen: ");
+            double entkomp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Bitte geben sie den Jahreszinssatz in % ein");
+            double jzs = Convert.ToDouble(Console.ReadLine());
+
+            dlagerb = (jab + jb) / 2;
+            us = entkomp / dlagerb;
+            dlagerd = 360 / us;
+            lagerz = (jzs * us) / 360;
+
+            Console.WriteLine("Durchschnittlicher Lagerbestand beträgt: " + dlagerb + " Komponenten");
+            Console.WriteLine("Die Umschlagshäufigkeit beträgt: " + us);
+            Console.WriteLine("Die Durchschnittliche Lagerdauer beträgt " + dlagerd + " Tage");
+            Console.WriteLine("Der Lagerzinssatz beträgt " + lagerz + "%");
+
+            Console.ReadKey();
+             
+
+
+
+
+
+        }
+    }
+}
